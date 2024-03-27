7c59b1437e242b53b8bdd745afa2bc7868d446f1
klemens-floege <klemens.floege@me.com>
2024-03-27 14:56:05 +0100

    SVN ensembles working

--- train/train_modellist.py
+++ train/train_modellist.py
@@ -22,9 +22,10 @@ def train_modellist(modellist, lr, num_epochs, train_dataloader, eval_dataloader
   K = RBF()
 
   parameters = [p for model in modellist for p in model.parameters()]
-  n_parameters_per_model = len([p for p in modellist[0].parameters()])
-  print(n_parameters_per_model)
-  print(len(parameters))
+  
+  n_parameters_per_model = sum(p.numel() for p in modellist[0].parameters() if p.requires_grad)
+  print('number of parameters per model', n_parameters_per_model)
+
 
   #print(type(W)
   optimizer = AdamW(params=parameters, lr=lr)
@@ -74,14 +75,10 @@ def train_modellist(modellist, lr, num_epochs, train_dataloader, eval_dataloader
         score_func_tensor = torch.cat(score_tensors).view(n_particles, -1)  # (n_particles, n_paramters_per_model)
 
         
-
-        
         particle_loader = data_utils.DataLoader(
             data_utils.TensorDataset(X, T), 
             batch_size=2
             )
-        
-        print('length dataloader', len(particle_loader))
         
         
         hessians_list = []
@@ -89,26 +86,19 @@ def train_modellist(modellist, lr, num_epochs, train_dataloader, eval_dataloader
         for i in range(n_particles):
             laplace_particle_model = FullLaplace(modellist[i], likelihood='regression')
             laplace_particle_model.fit(particle_loader)
-            
-
             Hessian = laplace_particle_model.posterior_precision
-
             hessians_list.append(Hessian)
 
         hessians_tensor = torch.cat(hessians_list, dim=0)
         hessians_tensor = hessians_tensor.reshape(n_particles, n_parameters_per_model, n_parameters_per_model) #(n_particles, n_parameters_per_model, n_parameters_per_model)
 
-        print('hessians_tensor shape: ', hessians_tensor.shape)
 
         
-        
-
         #compute curvature matrix
-        M = torch.mean(hessians_tensor, axis=0)
+        #M = torch.mean(hessians_tensor, axis=0)
+        M=None
 
-        gmlpt = score_func
-        GN_Hmlpt = hessians_tensor
-
+    
         
         param_tensors = [p.view(-1) for p in parameters]  # Flatten
         ensemble_parameters_tensor = torch.cat(param_tensors).view(n_particles, -1)  # Concatenate and reshape
@@ -119,10 +109,7 @@ def train_modellist(modellist, lr, num_epochs, train_dataloader, eval_dataloader
         grad_K = -autograd.grad(K_XX.sum(), ensemble_parameters_tensor)[0] #(n_particles, n_parameters_per_model)
 
         #print('KXX: ', K_XX.shape)
-        print('grad K: ', grad_K.shape)
-
-
-        
+        #print('grad K: ', grad_K.shape
 
         
         #v_svgd = -1 * contract('mn, mo -> no', kx, gmlpt) / self.nParticles + np.mean(gkx, axis=0)
@@ -133,37 +120,30 @@ def train_modellist(modellist, lr, num_epochs, train_dataloader, eval_dataloader
 
         
         #H = self._getSteinHessianPosdef(GN_Hmlpt, kx, gkx1)
+        H1 = torch.einsum("xy, xz, xbd -> yzbd", K_XX, K_XX, hessians_tensor) #(n_particles, n_particles, n_parametes_per_model, n_parametes_per_model)
+        H2 = torch.einsum('zi,zj -> ij', grad_K, grad_K) #(n_parametes_per_model, n_parametes_per_model))
+        H1[range(n_particles), range(n_particles)] += H2
+
+        N, _, D, _ = H1.shape  # Extract N and D from the shape of H
+        H_reshaped = H1.permute(0, 2, 1, 3).reshape(N*D, N*D)  # Reshape to [N*D, N*D] ,  #(n_particles*n_parametes_per_model, n_particles*n_parametes_per_model)
+        H =  H_reshaped / n_particles
 
         solve_method = 'CG'
         # solve_method = 'Cholesky'
 
         if solve_method == 'CG':
-            cg_maxiter = 50
-            #alphas = scipy.sparse.linalg.cg(hessians_tensor, v_svgd.flatten(), maxiter=cg_maxiter)[0].reshape(self.nParticles, self.DoF)
-            
             v_svgd_flat = v_svgd.flatten().view(-1, 1)  # Ensure it's a column vector
-
-            print('v_svg flat shape: ', v_svgd_flat.shape)
-
-            print('hessians_tensor: ', hessians_tensor.shape)
-
+            
             # Directly solve Ax = b using torch.linalg.solve if A is not too large and is positive definite
-            alphas = torch.linalg.solve(hessians_tensor, v_svgd_flat)
+            alphas = torch.linalg.solve(H, v_svgd_flat)
 
+            #alternative implementation<
+            #cg_maxiter = 50
+            #alphas = scipy.sparse.linalg.cg(hessians_tensor, v_svgd.flatten(), maxiter=cg_maxiter)[0]
+                    
+            alphas_reshaped = alphas.view(n_particles, -1) #(n_particles, n_parameters_per_model)
+            v_svn = torch.einsum('xd, xn -> nd', alphas_reshaped, K_XX) #(n_particles, n_parameters_per_model)
 
-
-            cg_maxiter = 50
-            alphas = scipy.sparse.linalg.cg(hessians_tensor, v_svgd.flatten(), maxiter=cg_maxiter)[0]
-            
-            print('alphas: ', alphas)
-            print('alphas shape: ', alphas.shape)
-
-            
-            # Reshape alphas if necessary, depending on subsequent usage
-            alphas_reshaped = alphas.view(n_Particles, self.DoF)
-            
-            
-            v_svn = contract('xd, xn -> nd', alphas, kx)
 
         elif solve_method == 'Cholesky':
             lamb = 0.01
@@ -171,16 +151,38 @@ def train_modellist(modellist, lr, num_epochs, train_dataloader, eval_dataloader
             UH = scipy.linalg.cholesky(H)
             v_svn = self._getSVN_direction(kx, v_svgd, UH)
         
-        update = v_svn * eps
+        #update = v_svn * eps
+            
+        for model, grads in zip(modellist, v_svn):
+            # Flatten all parameters of the model and their gradients
+            flat_params = torch.cat([p.view(-1) for p in model.parameters()])
+            
+            # Ensure we're iterating over the actual parameters of the model
+            param_iter = model.parameters()
+            
+            grad_idx = 0  # Start index for slicing gradients from grads
+            for param in param_iter:
+                # Number of elements in the current parameter
+                num_param_elements = param.numel()
+                
+                # Slice the gradient for the current parameter
+                grad_for_param = grads[grad_idx:grad_idx + num_param_elements].view(param.size())
+                
+                # Manually assign the gradient for the current parameter
+                if param.grad is not None:
+                    param.grad = grad_for_param
+                else:
+                    param.grad = grad_for_param.clone()
+                
+                # Update the start index for the next parameter
+                grad_idx += num_param_elements
+
+        
     
-
-
-        
-        
-
+        optimizer.step()
         
         if step == 0:          
-          loss_str = ', '.join([f'Loss {i+1} = {losses[i][0]:.4f}' for i in range(len(losses))])
+          loss_str = ', '.join([f'Loss {i} = {loss[i].item():.4f}' for i in range(loss.shape[0])])
           print(f'Train Epoch {epoch}, {loss_str}')
     
     # Evaluation loop
@@ -193,23 +195,15 @@ def train_modellist(modellist, lr, num_epochs, train_dataloader, eval_dataloader
     for step, batch in enumerate(tqdm(eval_dataloader)):
         with torch.no_grad():
             inputs, targets = batch
-            outputs = ensemble.forward(inputs)
             
-            #average prediction over all particles
-            ensemble_pred = torch.mean(outputs[0], dim=0) 
-            
-            #mse = mse_loss(ensemble_pred, targets)
-
-            #loss = 0.5*torch.mean(F.mse_loss(ensemble_pred, targets, reduction='none'), 1)
-            #print(ensemble_pred.shape)
-            #print(targets.shape)
-            #print((ensemble_pred.expand_as(targets)-targets)**2)
+            pred_list = []
+            for i in range(n_particles):
+                pred_list.append(modellist[i].forward(X))
+            pred = torch.cat(pred_list, dim=0)
+            pred_reshaped = pred.view(n_particles, -1, dim_problem)
+            ensemble_pred = torch.mean(pred_reshaped, dim=0) 
+                        
             loss = (ensemble_pred.expand_as(targets)-targets)**2
-
-            #print(a.sum())
-
-            #loss = 0.5*torch.mean((ensemble_pred.expand_as(targets)-targets)**2,1)
-            
 
             
             total_mse = loss.sum() 
