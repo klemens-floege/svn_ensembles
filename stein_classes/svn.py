0181fdf2e9c7e4d46116b39c0c96217d1ce471d4
klemens-floege <klemens.floege@me.com>
2024-04-11 17:58:19 +0200

    move Kron to function to stein utils

--- stein_classes/svn.py
+++ stein_classes/svn.py
@@ -6,120 +6,8 @@ import numpy as np
 
 from laplace import FullLaplace, KronLaplace, DiagLaplace, LowRankLaplace
 
-from stein_classes.stein_utils import calc_loss
+from stein_classes.stein_utils import calc_loss, hessian_matvec
 
-def apply_kron_eigen_decomp(eigenvalues_layer, eigenvectors_layer, x_partition):
-    """
-    Applies the Kronecker product operation using eigen decomposition for a single layer.
-    eigenvalues_layer: A tuple containing two tensors of eigenvalues for K and Q.
-    eigenvectors_layer: A tuple containing two tensors of eigenvectors for K and Q.
-    x_partition: The part of x corresponding to this layer.
-    
-    Returns the result of the operation for this layer.
-    """
-    # Unpack eigenvalues and eigenvectors
-    lambda_K, lambda_Q = eigenvalues_layer
-    V_K, V_Q = eigenvectors_layer
-    
-    # Assume x_partition is appropriately reshaped for this layer
-    # The actual reshaping will depend on the dimensions of V_K and V_Q
-
-    lambda_K = lambda_K.float()
-    lambda_Q = lambda_Q.float()
-    V_K = V_K.float()
-    V_Q = V_Q.float()
-    x_partition = x_partition.float()
-    
-    # Compute using eigen decompositions (this is a simplified conceptual approach)
-    result = torch.kron(torch.matmul(V_K, torch.matmul(torch.diag(lambda_K), V_K.T)),
-                        torch.matmul(V_Q, torch.matmul(torch.diag(lambda_Q), V_Q.T))) @ x_partition
-    
-    return result
-
-
-
-def compute_KFACx(kron_decomposed, x):
-    '''
-    This function computes the matrix vector product A * x, for a matrix A which is given in kron.decomposed format
-    '''
-    
-    KFACx_result = []
-    x_index = 0  # Tracks our position in x as we partition it
-
-    eigenvalues = kron_decomposed.eigenvalues
-    eigenvectors = kron_decomposed.eigenvectors
-
-    counter_parameters = 0
-    for eigenvalues_layer, eigenvectors_layer in zip(eigenvalues, eigenvectors):
-        
-        #hande layer weights which are given as Kronecker factors
-        if len(eigenvalues_layer) > 1:
-            
-            #compute number of elements in layer
-            V_K, V_Q = eigenvectors_layer
-            num_elements_K = V_K.shape[0]
-            num_elements_Q = V_Q.shape[0]
-            partition_size = num_elements_K * num_elements_Q
-
-            # Extract the relevant partition of x and compute matmul with Kronecker product
-            x_partition = x[x_index:x_index + partition_size]
-            x_partition = torch.tensor(x_partition)
-            x_partition.float()
-            layer_result = apply_kron_eigen_decomp(eigenvalues_layer, eigenvectors_layer, x_partition)
-            
-        # Handle bias, which is represented an Eigendecomposition of full matrix
-        else:
-            
-            lambda_bias = eigenvalues_layer[0]
-            V_bias = eigenvectors_layer[0]
-            partition_size = lambda_bias.numel()
-      
-            x_partition = torch.tensor(x[x_index:x_index + partition_size], dtype=torch.float32)
-            V_bias = V_bias.clone().detach().to(dtype=torch.float32)
-            lambda_bias = lambda_bias.clone().detach().to(dtype=torch.float32)
-
-            layer_result = torch.matmul(V_bias, torch.matmul(torch.diag(lambda_bias), V_bias.T @ x_partition))
-        
-        counter_parameters += partition_size
-
-        
-        KFACx_result.append(layer_result.flatten())
-        x_index += partition_size
-
-    # Concatenate all results into a single vector
-    result_tensor = torch.cat(KFACx_result)
-
-    if counter_parameters != x.shape[0]:
-        raise ValueError('The KFAC times vector multiplcation did not work. Total sum of opearted elements not euqal to size of vector')
-    
-    return result_tensor
-
-def hessian_matvec(input, K_XX, kron_list, H2, n_parameters):
-
-        n_particles = len(kron_list)
-        result = torch.zeros_like(torch.tensor(input))
-
-
-        # Now, use transformed_eigenvalues for further operations or aggregation
-        # This involves broadcasting K_XX across the dimensions where it is needed
-        weights = K_XX[:, :, None] * K_XX[:, None, :]  # Shape: [n_particles, n_particles, n_particles]
-        
-
-        for y in range(n_particles):
-            for z in range(n_particles):
-                for x in range(n_particles):
-                    
-                    current_parameter_vector = input[n_parameters*z: n_parameters*(z+1)]
-
-                    result[n_parameters*y: n_parameters*(y+1)] += weights[x, y, z] * compute_KFACx(kron_list[x], current_parameter_vector)
-
-                    if z == y:
-                        if isinstance(current_parameter_vector, np.ndarray):
-                            current_parameter_vector = torch.from_numpy(current_parameter_vector).float()
-                        result[n_parameters*y: n_parameters*(y+1)] += torch.matmul(H2[y], current_parameter_vector)  
-
-
-        return result.detach().numpy()
 
 def apply_SVN(modellist, parameters, 
               batch, train_dataloader, kernel, device, cfg):
@@ -253,7 +141,7 @@ def apply_SVN(modellist, parameters,
 
         v_svgd_numpy = v_svgd.detach().cpu().flatten().numpy()
 
-        alphas, _ = scipy.sparse.linalg.cg(H_numpy, v_svgd_numpy, maxiter=cg_maxiter)
+        alphas, _ = scipy.sparse.linalg.cg(H_op, v_svgd_numpy, maxiter=cg_maxiter)
         alphas = torch.tensor(alphas, dtype=torch.float32).reshape(n_particles, n_parameters).to(device)
                 
         alphas_reshaped = alphas.view(n_particles, -1) #(n_particles, n_parameters)
