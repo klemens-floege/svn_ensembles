6671403f7886864c688705db06c35e05fe84c179
klemens-floege <klemens.floege@me.com>
2024-04-24 17:26:19 +0200

    update rq.txt

--- stein_classes/ensemble.py
+++ stein_classes/ensemble.py
@@ -6,7 +6,7 @@ from stein_classes.stein_utils import calc_loss
 def apply_Ensemble(modellist, parameters, 
               batch, train_dataloader, kernel, device, cfg, optimizer):
         
-    loss, log_prob = calc_loss(modellist, batch, train_dataloader, cfg, device)
+    loss, log_prob = calc_loss(modellist, batch, train_dataloader, cfg, device) #[n_particles, bsz]
 
     #print('loss', loss)
 
